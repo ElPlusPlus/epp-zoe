65da1bd50eb1b5fef4d0a39173487b8863616615
Filip Hultgren <hultgren.filip@gmail.com>
2026-02-04 17:58:43 +0100

    u

--- pyzoe/definition/register_values.py
+++ pyzoe/definition/register_values.py
@@ -2,4 +2,4 @@ from pyzoe.definition.models import ModbusRegister, ModbusType, ModbusUnit
 
 serial_number = ModbusRegister(4142, 12, 3, ModbusType.STRING, ModbusUnit.NONE, gain=1)
 
-soc = ModbusRegister(20109, 1, 3, ModbusType.U16, ModbusUnit.PERCENTAGE, gain=0.1)
+soc = ModbusRegister(20109, 1, 4, ModbusType.U16, ModbusUnit.PERCENTAGE, gain=0.1)
