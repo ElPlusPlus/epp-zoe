b97dc9911b9695a91b54efa47a85bc83dc0dbf46
Filip Hultgren <hultgren.filip@gmail.com>
2026-02-04 17:53:03 +0100

    u

--- pyzoe/definition/register_values.py
+++ pyzoe/definition/register_values.py
@@ -1,3 +1,3 @@
 from pyzoe.definition.models import ModbusRegister, ModbusType, ModbusUnit
 
-serial_number = ModbusRegister(10000, 8, 3, ModbusType.STRING, ModbusUnit.NONE, gain=1)
+serial_number = ModbusRegister(4127, 12, 3, ModbusType.STRING, ModbusUnit.NONE, gain=1)
