585d372515e790b47ab63dd9afdb253731cc17dc
Filip Hultgren <hultgren.filip@gmail.com>
2026-02-05 07:33:27 +0100

    u

--- pyzoe/definition/register_values.py
+++ pyzoe/definition/register_values.py
@@ -3,7 +3,7 @@ from pyzoe.definition.models import ModbusRegister, ModbusType, ModbusUnit
 board_serial_number = ModbusRegister(4142, 12, 3, ModbusType.STRING, ModbusUnit.NONE)
 serial_number = ModbusRegister(4142, 12, 3, ModbusType.STRING, ModbusUnit.NONE)
 
-soc = ModbusRegister(20109, 1, 3, ModbusType.U16, ModbusUnit.PERCENTAGE, gain=0.1)
+soc = ModbusRegister(24681, 1, 3, ModbusType.U16, ModbusUnit.PERCENTAGE, gain=0.1)
 pcs_running_state = ModbusRegister(40005, 1, 3, ModbusType.U16, ModbusUnit.NONE)
 
 grid_frequency = ModbusRegister(40018, 1, 3, ModbusType.U16, ModbusUnit.NONE, gain=0.01)
